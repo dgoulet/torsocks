8448afed1c904dce08c3d8a3b70816e2dbd9c636
hoganrobert <hoganrobert@2b4a61ce-a206-11dd-9a14-c1645b63b0c6>
2008-06-22 12:20:37 +0000

    Add create-release.sh
    git-svn-id: https://torsocks.googlecode.com/svn/trunk@20 2b4a61ce-a206-11dd-9a14-c1645b63b0c6

---
+++ create-release.sh
@@ -0,0 +1,21 @@
+export CVS_RSH=ssh
+export VN=1.0-alpha
+export VER=torsocks-$VN
+export TAG=v1_0-alpha
+cd ..
+TOPDIR=$PWD
+mkdir TorsocksReleases
+cd TorsocksReleases
+INSTALLDIR=$PWD
+
+#create source package
+cvs -z3 -d:ext:hoganrobert@tork.cvs.sourceforge.net:/cvsroot/tork export -r $TAG torsocks
+cd torsocks
+make -f Makefile.cvs
+rm -rf autom4te.cache
+cd ..
+mv torsocks $VER
+tar jcvf $VER.tar.bz2 $VER
+tar zcvf $VER.tar.gz $VER
+gpg -sba $VER.tar.bz2
+gpg -sba $VER.tar.gz
