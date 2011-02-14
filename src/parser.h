f7b902f88d97c7a54b7c23f669752eb49867055b
Robert Hogan <robert@roberthogan.net>
2011-02-14 21:01:28 +0000

    Whitespace cleanup in parser.h

--- src/parser.h
+++ src/parser.h
@@ -30,23 +30,23 @@
 
 /* Structure representing one server specified in the config */
 struct serverent {
-    int lineno; /* Line number in conf file this path started on */
-    char *address; /* Address/hostname of server */
-    int port; /* Port number of server */
-    int type; /* Type of server (4/5) */
-    char *defuser; /* Default username for this socks server */
-    char *defpass; /* Default password for this socks server */
+    int lineno;               /* Line number in conf file this path started on */
+    char *address;            /* Address/hostname of server */
+    int port;                 /* Port number of server */
+    int type;                 /* Type of server (4/5) */
+    char *defuser;            /* Default username for this socks server */
+    char *defpass;            /* Default password for this socks server */
     struct netent *reachnets; /* Linked list of nets from this server */
-    struct serverent *next; /* Pointer to next server entry */
+    struct serverent *next;   /* Pointer to next server entry */
 };
 
 /* Structure representing a network */
 struct netent {
-   struct in_addr localip; /* Base IP of the network */
-   struct in_addr localnet; /* Mask for the network */
-   unsigned long startport; /* Range of ports for the */
-   unsigned long endport;   /* network                */
-   struct netent *next; /* Pointer to next network entry */
+   struct in_addr localip;    /* Base IP of the network */
+   struct in_addr localnet;   /* Mask for the network */
+   unsigned long startport;   /* Range of ports for the */
+   unsigned long endport;     /* network                */
+   struct netent *next;       /* Pointer to next network entry */
 };
 
 /* Structure representing a complete parsed file */
