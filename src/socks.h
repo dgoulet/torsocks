91046642c2efa6bed622f524b95919563d2a40a5
Robert Hogan <robert@roberthogan.net>
2011-02-19 13:20:21 +0000

    Split SOCKS functions into separate file

--- src/socks.h
+++ src/socks.h
@@ -18,13 +18,14 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-/* torsocks.h - Structures used by torsocks to form SOCKS requests */
+/* socks.h - Structures used by torsocks to form SOCKS requests */
 
-#ifndef _TORSOCKS_H
+#ifndef _SOCKS_H
 
-#define _TORSOCKS_H 1
+#define _SOCKS_H 1
 
 #include "parser.h"
+#include "dead_pool.h"
 
 /* Structure representing a socks connection request */
 struct sockreq {
@@ -59,7 +60,7 @@ struct connreq {
    /* Next state to go to when the send or receive is finished */
    int nextstate;
 
-   /* When connections fail but an error number cannot be reported 
+   /* When connections fail but an error number cannot be reported
     * because the socket is non blocking we keep the connreq struct until
     * the status is queried with connect() again, we then return
     * this value */
@@ -84,21 +85,32 @@ struct connreq {
 #define SENDING 3
 #define RECEIVING 4
 #define SENTV4REQ 5
-#define GOTV4REQ 6 
-#define SENTV5METHOD 7 
+#define GOTV4REQ 6
+#define SENTV5METHOD 7
 #define GOTV5METHOD 8
 #define SENTV5AUTH 9
 #define GOTV5AUTH 10
 #define SENTV5CONNECT 11
 #define GOTV5CONNECT 12
-#define DONE 13 
-#define FAILED 14 
-   
+#define DONE 13
+#define FAILED 14
+
 /* Flags to indicate what events a socket was select()ed for */
 #define READ (1<<0)
 #define WRITE (1<<1)
 #define EXCEPT (1<<2)
 #define READWRITE (READ|WRITE)
 #define READWRITEEXCEPT (READ|WRITE|EXCEPT)
+
+/* Global Declarations */
+static dead_pool *pool = NULL;
+static struct connreq *requests = NULL;
+
+struct connreq *new_socks_request(int sockid, struct sockaddr_in *connaddr,
+                                         struct sockaddr_in *serveraddr,
+                                         struct serverent *path);
+void kill_socks_request(struct connreq *conn);
+struct connreq *find_socks_request(int sockid, int includefailed);
+int handle_request(struct connreq *conn);
 
 #endif
