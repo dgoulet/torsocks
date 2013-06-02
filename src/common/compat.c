dbab1272a03ba4a5cd63ca641b6a0e8d55f37306
David Goulet <dgoulet@ev0ke.net>
2013-06-02 14:52:01 -0400

    Add compat.c/.h defining a compat tsocks mutex
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

---
+++ src/common/compat.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2013 - David Goulet <dgoulet@ev0ke.net>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <assert.h>
+
+#include "compat.h"
+
+#ifdef __linux__
+
+/*
+ * Initialize a pthread mutex. This never fails.
+ */
+void tsocks_mutex_init(tsocks_mutex_t *m)
+{
+	assert(m);
+	pthread_mutex_init(&m->mutex, NULL);
+}
+
+/*
+ * Destroy a pthread mutex. This never fails.
+ */
+void tsocks_mutex_destroy(tsocks_mutex_t *m)
+{
+	assert(m);
+	pthread_mutex_destroy(&m->mutex);
+}
+
+/*
+ * Use pthread mutex lock and assert on any error.
+ */
+void tsocks_mutex_lock(tsocks_mutex_t *m)
+{
+	int ret;
+
+	assert(m);
+	ret = pthread_mutex_lock(&m->mutex);
+	/*
+	 * Unable to lock the mutex could lead to undefined behavior and potential
+	 * security issues. Stop everything so torsocks can't continue.
+	 */
+	assert(!ret);
+}
+
+/*
+ * Use pthread mutex unlock and assert on any error.
+ */
+void tsocks_mutex_unlock(tsocks_mutex_t *m)
+{
+	int ret;
+
+	assert(m);
+	ret = pthread_mutex_unlock(&m->mutex);
+	/*
+	 * Unable to unlock the mutex could lead to undefined behavior and potential
+	 * security issues. Stop everything so torsocks can't continue.
+	 */
+	assert(!ret);
+}
+
+#endif /* __linux__ */
---
+++ src/common/compat.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2013 - David Goulet <dgoulet@ev0ke.net>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef TORSOCKS_COMPAT_H
+#define TORSOCKS_COMPAT_H
+
+#ifdef __linux__
+
+#include <pthread.h>
+
+typedef struct tsocks_mutex_t {
+	pthread_mutex_t mutex;
+} tsocks_mutex_t;
+
+/* Define a tsock mutex variable with the mutex statically initialized. */
+#define TSOCKS_INIT_MUTEX(name) \
+	tsocks_mutex_t name = { .mutex = PTHREAD_MUTEX_INITIALIZER }
+
+void tsocks_mutex_init(tsocks_mutex_t *m);
+void tsocks_mutex_destroy(tsocks_mutex_t *m);
+void tsocks_mutex_lock(tsocks_mutex_t *m);
+void tsocks_mutex_unlock(tsocks_mutex_t *m);
+
+#endif /* __linux__ */
+
+#endif /* TORSOCKS_COMPAT_H */
