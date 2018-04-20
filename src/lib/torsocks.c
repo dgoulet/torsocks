e4601e0f89b47c5aff9df30238a10224e8bd04a6
David Goulet <dgoulet@ev0ke.net>
2018-04-20 09:01:17 -0400

    accept4: Initialize libc symbol early
    Fixes #17618
    
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- src/lib/torsocks.c
+++ src/lib/torsocks.c
@@ -234,8 +234,9 @@ static void init_libc_symbols(void)
 	tsocks_libc_socket = dlsym(libc_ptr, LIBC_SOCKET_NAME_STR);
 	tsocks_libc_syscall = dlsym(libc_ptr, LIBC_SYSCALL_NAME_STR);
 	tsocks_libc_execve = dlsym(libc_ptr, LIBC_EXECVE_NAME_STR);
-	if (!tsocks_libc_connect || !tsocks_libc_close || !tsocks_libc_socket
-			|| !tsocks_libc_syscall || !tsocks_libc_execve) {
+	tsocks_libc_accept4 = dlsym(libc_ptr, LIBC_ACCEPT4_NAME_STR);
+	if (!tsocks_libc_connect || !tsocks_libc_close || !tsocks_libc_socket ||
+			!tsocks_libc_syscall || !tsocks_libc_execve || ! tsocks_libc_accept4) {
 		ERR("Unable to lookup symbols in " LIBC_NAME "(%s)", dlerror());
 		goto error;
 	}
