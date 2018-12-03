4ad9882e72386001d7b7c5b07f9f0077cf166fa9
Yuri <yuri@rawbw.com>
2018-12-02 16:46:12 -0800

    FreeBSD patches

--- src/common/compat.h
+++ src/common/compat.h
@@ -214,6 +214,10 @@ void tsocks_once(tsocks_once_t *o, void (*init_routine)(void));
 #define TSOCKS_NR_GETPEERNAME SYS_getpeername
 #define TSOCKS_NR_LISTEN    SYS_listen
 #define TSOCKS_NR_RECVMSG   SYS_recvmsg
+#if defined(__FreeBSD__)
+#define TSOCKS_NR_GETPID    SYS_getpid
+#define TSOCKS_NR_FORK      SYS_fork
+#endif
 
 #endif /* __FreeBSD__, __FreeBSD_kernel__, __darwin__, __NetBSD__ */
 
